06edda6cee112efd12be96d672e2996c28515741
akskflwn <akskflwn@gmail.com>
2022-09-22 19:58:46 +0900

    화면 구현

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,15 +21,16 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
+    //==검색 쿼리==Querydsl//
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//        return em.createQuery("select o from Order o join o.member m " +
+//                "where o.status = :status" +
+//                "and m.name like :name",Order.class)
+//                .setParameter("status",orderSearch.getOrderStatus())
+//                .setParameter("name",orderSearch.getMemberName())
+//                .setMaxResults(1000)//최대 100건
+//                .getResultList();
+//    }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
-
-        return em.createQuery("select o from Order o join o.member m " +
-                "where o.status = :status" +
-                "and m.name like :name",Order.class)
-                .setParameter("status",orderSearch.getOrderStatus())
-                .setParameter("name",orderSearch.getMemberName())
-                .setMaxResults(1000)//최대 100건
-                .getResultList();
-    }
 }
