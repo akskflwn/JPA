5fbf95951b6786641b1597a348d84b63b7e7dc48
akskflwn <akskflwn@gmail.com>
2022-09-23 12:36:29 +0900

    검색기능 완성 페이지 완성2

--- jpashop/src/main/java/jpabook/jpashop/Service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/Service/OrderService.java
@@ -4,9 +4,12 @@ import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -56,8 +59,8 @@ public class OrderService {
         order.cancel();
     }
     //검색
-//    public List<Order> findOrders(OrderSearch orderSearch){
-//        return orderRepository.findOne(orderSearch);
-//    }
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
 
 }
